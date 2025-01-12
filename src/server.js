165d19c2e27822e71da76b755cb4bfde7257d000
deeepak-me <deepak.krishnan.e@gmail.com>
2025-01-12 21:13:12 +0530

    feat: added path

---
+++ src/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Get the file path from the URL of the current module
+const __filename = fileURLToPath(import.meta.url);
+
+// Get the directory name from the file path
+const __dirname = dirname(__filename);
+
+// serve the HTML file from the /public directory
+// Tells express to serve all files from the public folder as static assets /file. any request for the css files will be resolved to the public directory.
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+// serving up the HTML file from the  /public directory
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`server has started on port: ${PORT}`);
+});
