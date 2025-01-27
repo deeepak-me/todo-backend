1cac7214fb27f19e64e2622fbf81aad36a89a5f2
deeepak-me <deepak.krishnan.e@gmail.com>
2025-01-27 19:53:12 +0530

    feat: updating server with routes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/authRoutes.js";
+import todoRoutes from "./routes/todoRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,6 +13,9 @@ const __filename = fileURLToPath(import.meta.url);
 // Get the directory name from the file path
 const __dirname = dirname(__filename);
 
+//Middlewares
+app.use(express.json());
+
 // serve the HTML file from the /public directory
 // Tells express to serve all files from the public folder as static assets /file. any request for the css files will be resolved to the public directory.
 
@@ -20,6 +25,10 @@ app.use(express.static(path.join(__dirname, "../public")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
+//Routes
+app.use("/auth", authRoutes);
+app.use("/todos", todoRoutes);
 
 app.listen(PORT, () => {
   console.log(`server has started on port: ${PORT}`);
