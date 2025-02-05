b08d3cd5c78b68d60654ef416f5c66abf6e71bb0
deeepak-me <deepak.krishnan.e@gmail.com>
2025-02-05 23:58:34 +0530

    refactor: small changes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -35,26 +35,28 @@ router.post("/register", (req, res) => {
     res.sendStatus(503);
   }
 });
+
 router.post("/login", (req, res) => {
   const { username, password } = req.body;
+
   try {
     const getUser = db.prepare("SELECT * FROM users WHERE username = ?");
     const user = getUser.get(username);
 
     if (!user) {
-      return res.status(404).send({ message: "User Not Found!" });
+      return res.status(404).send({ message: "User Not Found!!" });
     }
 
     const passwordIsValid = bcrypt.compareSync(password, user.password);
     if (!passwordIsValid) {
-      return res.status(401).send({ message: "Invalid Password" });
+      return res.status(401).send({ message: "Invalid Password!!" });
     }
 
     const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
       expiresIn: "24h",
     });
     res.json({ token });
-  } catch (error) {
+  } catch (err) {
     console.log(err.message);
     res.sendStatus(503);
   }
